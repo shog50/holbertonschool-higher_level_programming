9ae631e71e9b126cd8067e1999e84ae463458478
shog50 <10709@holbertonstudents.com>
2025-05-07 14:31:47 +0000

    When I was having that alphabet soup, I never thought that it would pay off

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,3 +1,6 @@
 #!/usr/bin/python3
 
-print("{}".format("".join(chr(i) for i in range(97, 123) if i not in (ord('q'), ord('e')))), end="")
+print("{}".format(
+    "".join(chr(i) for i in range(97, 123) if i not in (ord('q'), ord('e')))
+), end="")
+
