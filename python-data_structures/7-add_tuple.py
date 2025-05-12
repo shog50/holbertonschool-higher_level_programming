44762647a627385fada3ddcf310e3bc3dd8255e6
shog50 <10709@holbertonstudents.com>
2025-05-12 12:20:54 +0000

    Tuples addition

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 def add_tuple(tuple_a=(), tuple_b=()):
-    """Adds two tuples element-wise, ensuring they have at least two elements."""
+    """Adds 2 tuples element-wise, ensuring they have at least 2 elements."""
     a1, a2 = (tuple_a + (0, 0))[:2]
     b1, b2 = (tuple_b + (0, 0))[:2]
 
