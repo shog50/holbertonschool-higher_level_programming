c546c95490bb63155831be450d8395e91bd3bc0a
shog50 <10709@holbertonstudents.com>
2025-05-12 12:27:20 +0000

    Find the max

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -4,10 +4,8 @@ def max_integer(my_list=[]):
     """Finds the largest integer in a list."""
     if not my_list:
         return None  # Return None if the list is empty
-    
     max_value = my_list[0]  # Initialize max_value with the first element
     for num in my_list:
         if num > max_value:
             max_value = num  # Update max_value if a larger number is found
-    
     return max_value
