c6983592333dc22fe605bf665a27352eb1ea569e
shog50 <10709@holbertonstudents.com>
2025-05-16 16:03:22 +0000

    Divide a list

--- python-exceptions/4-list_division.py
+++ python-exceptions/4-list_division.py
@@ -6,10 +6,14 @@ def list_division(my_list_1, my_list_2, list_length):
         try:
             if i >= len(my_list_1) or i >= len(my_list_2):
                 raise IndexError("out of range")
-            if not isinstance(my_list_1[i], (int, float))
-            or not isinstance(my_list_2[i], (int, float)):
+            is_valid_type1 = isinstance(my_list_1[i], (int, float))
+            is_valid_type2 = isinstance(my_list_2[i], (int, float))
+
+            if not is_valid_type1 or not is_valid_type2:
                 raise TypeError("wrong type")
+
             result.append(my_list_1[i] / my_list_2[i])
+
         except ZeroDivisionError:
             print("division by 0")
             result.append(0)
@@ -21,4 +25,5 @@ def list_division(my_list_1, my_list_2, list_length):
             result.append(0)
         finally:
             pass
+
     return result
