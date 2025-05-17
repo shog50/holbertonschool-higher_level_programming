4c14341b2d5a7bd46b03847278480f90644b061e
shog50 <10709@holbertonstudents.com>
2025-05-17 19:41:04 +0000

    say my name

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 """
-This module contains the function say_my_name
-that prints a formatted full name.
+This module contains the function say_my_name that prints a formatted full name
 """
 
 
@@ -16,12 +15,11 @@ def say_my_name(first_name, last_name=""):
     Raises:
         TypeError: if first_name or last_name is not a string
     """
-    if not isinstance(first_name, str):
+
+    if type(first_name) is not str:
         raise TypeError("first_name must be a string")
-    if not isinstance(last_name, str):
+
+    if type(last_name) is not str:
         raise TypeError("last_name must be a string")
 
-    if last_name:
-        print("My name is {} {}".format(first_name, last_name))
-    else:
-        print("My name is {} ".format(first_name))
+    print("My name is {} {}".format(first_name, last_name))
