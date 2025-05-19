38011dfaf72d2aa5d427c227ec07bba65a836715
shog50 <10709@holbertonstudents.com>
2025-05-19 12:02:42 +0000

    My first square

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+"""This module defines an empty Square class."""
+
+
 class Square:
-    """An empty class that defines a square."""
+    """Represents a square without any properties or methods."""
     pass
