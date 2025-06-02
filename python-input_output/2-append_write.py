391eec63c25ac1608baef7dbd2430d91fdda6f34
shog50 <10709@holbertonstudents.com>
2025-06-02 15:01:34 +0000

    Append to a file

--- python-input_output/2-append_write.py
+++ python-input_output/2-append_write.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """
-This module provides a function to append a string to the end of a UTF-8 text file.
+This module provides 
+a function to append a string to the end of a UTF-8 text file.
 """
 
 
