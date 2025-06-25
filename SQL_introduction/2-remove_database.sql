9a20f192663ea7a3b3e9e1292a49ef44024a73d4
shog50 <10709@holbertonstudents.com>
2025-06-25 22:54:43 +0000

    2, 3

---
+++ SQL_introduction/2-remove_database.sql
@@ -0,0 +1,4 @@
+-- Task 2: Delete the database hbtn_0c_0 in your MySQL server.
+-- This ensures the script doesn't fail if the DB doesn't exist
+DROP DATABASE IF EXISTS hbtn_0c_0;
+
---
+++ SQL_introduction/3-list_tables.sql
@@ -0,0 +1,4 @@
+-- Task 3: lists all the tables of a database in your MySQL server.
+-- The database name is passed externally, not hardcoded here
+SHOW TABLES;
+
