24d0366f9b599b71b718ab17db5ba7f7c803bf48
shog50 <10709@holbertonstudents.com>
2025-06-25 22:59:39 +0000

    First table

---
+++ SQL_introduction/4-first_table.sql
@@ -0,0 +1,7 @@
+-- Task 4: Create table 'first_table' if it does not exist
+-- The table will have columns 'id' (INT) and 'name' (VARCHAR 256)
+CREATE TABLE IF NOT EXISTS first_table (
+    id INT,
+    name VARCHAR(256)
+);
+
