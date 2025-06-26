ece3e0c6c08743c04749fff619d74b27690f56d7
shog50 <10709@holbertonstudents.com>
2025-06-26 13:52:59 +0000

    Count 89

---
+++ SQL_introduction/8-count_89.sql
@@ -0,0 +1,5 @@
+-- Task: Display the number of records with id = 89 in the table first_table
+-- This query counts how many rows have id equal to 89
+
+SELECT COUNT(*) FROM first_table WHERE id = 89;
+
