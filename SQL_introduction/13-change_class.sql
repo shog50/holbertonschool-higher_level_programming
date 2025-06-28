2abfba70fc16e0bcfb90422191e284ae7cfdfc52
shog50 <10709@holbertonstudents.com>
2025-06-28 18:40:21 +0000

    Score too low

---
+++ SQL_introduction/13-change_class.sql
@@ -0,0 +1,6 @@
+-- Task: Delete all records with a score <= 5 from 'second_table'
+
+-- This query deletes records where score is less than or equal to 5
+DELETE FROM second_table
+WHERE score <= 5;
+
