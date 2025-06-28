7b7c5a426eee2975a86818e14c955a823ba8199f
shog50 <10709@holbertonstudents.com>
2025-06-28 19:22:20 +0000

    Cities by States

---
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -0,0 +1,8 @@
+-- Task: List all cities with their state names using one SELECT and JOIN, ordered by city id
+
+SELECT cities.id, cities.name, states.name
+FROM cities
+INNER JOIN states
+ON cities.state_id = states.id
+ORDER BY cities.id ASC;
+
