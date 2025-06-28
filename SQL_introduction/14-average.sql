4a456cb116aea508c1631a6b48b3bf5bcdde6d84
shog50 <10709@holbertonstudents.com>
2025-06-28 18:42:07 +0000

    Average

---
+++ SQL_introduction/14-average.sql
@@ -0,0 +1,6 @@
+-- Task: Compute the average score of all records in 'second_table'
+
+-- This query calculates the average score and names the result column as 'average'
+SELECT AVG(score) AS average
+FROM second_table;
+
