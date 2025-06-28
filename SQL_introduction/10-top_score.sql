3bd5fd33a64d89cea49ea369b958d815e6e69ff7
shog50 <10709@holbertonstudents.com>
2025-06-28 18:32:56 +0000

    List by best

---
+++ SQL_introduction/10-top_score.sql
@@ -0,0 +1,7 @@
+-- Task: List all records from 'second_table' ordered by score (highest first)
+
+-- This query selects score and name from second_table, ordered by score descending
+SELECT score, name
+FROM second_table
+ORDER BY score DESC;
+
