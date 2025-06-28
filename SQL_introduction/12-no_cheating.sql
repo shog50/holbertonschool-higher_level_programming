7bf12046d7c46757394f289af385ca737b049be0
shog50 <10709@holbertonstudents.com>
2025-06-28 18:38:26 +0000

    Cheating is bad

---
+++ SQL_introduction/12-no_cheating.sql
@@ -0,0 +1,7 @@
+-- Task: Update the score of 'Bob' to 10 in 'second_table' using the name field only
+
+-- This query updates Bob's score to 10
+UPDATE second_table
+SET score = 10
+WHERE name = 'Bob';
+
