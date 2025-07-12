e17af30f69f7a17174e5e3dcd151e3be23ad0657
shog50 <10709@holbertonstudents.com>
2025-07-12 15:49:44 +0000

    Second biggest!

---
+++ javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+const args = process.argv.slice(2).map(x => parseInt(x));
+
+if (args.length <= 1) {
+  console.log(0);
+} else {
+  
+  const uniqueArgs = [...new Set(args)];
+  if (uniqueArgs.length <= 1) {
+    console.log(0);
+  } else {
+   
+    uniqueArgs.sort((a, b) => b - a);
+    console.log(uniqueArgs[1]);
+  }
+}
+
